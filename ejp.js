4780c23a9fa0400139866831f298d703b74fb493
whiteplanet <bigwplanet@gmail.com>
2016-06-25 16:26:25 +0300

    Update ejp.js

--- ejp.js
+++ ejp.js
@@ -1,13 +1,13 @@
 {
-  'id': "6001",
-  'text': "Hello",
-  'artwork': "https://widgets.itunes.apple.com/images/app_search_intro.png",
-  'button1': "Dismiss",
-  'button2': "Remind me later",
-  'button3': "Download",
-  'button1_action': "dismiss",
-  'button2_action': "remind",
-  'button3_action': "open",
-  'download_link': "https://itunes.apple.com/us/app/app-for-instagram-instant/id954338382?mt=12&uo=2&at=11l4Vw&ct=test1",
-  'remind_delay': 172800
+  "id": "6001",
+  "text": "Hello",
+  "artwork": "https://widgets.itunes.apple.com/images/app_search_intro.png",
+  "button1": "Dismiss",
+  "button2": "Remind me later",
+  "button3": "Download",
+  "button1_action": "dismiss",
+  "button2_action": "remind",
+  "button3_action": "open",
+  "download_link": "https://itunes.apple.com/us/app/app-for-instagram-instant/id954338382?mt=12&uo=2&at=11l4Vw&ct=test1",
+  "remind_delay": 172800
 }
