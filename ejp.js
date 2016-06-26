31b211a240e0bec4f85ac0a9fd7240ce0973c041
whiteplanet <bigwplanet@gmail.com>
2016-06-26 11:36:50 +0300

    Update ejp.js

--- ejp.js
+++ ejp.js
@@ -1,2 +1,13 @@
 {
+  "id": "6001",
+  "text": "Happy with Emoji app for your Mac? Download our new app!",
+  "artwork": "http://a5.mzstatic.com/us/r30/Purple60/v4/73/a1/b3/73a1b361-d7b0-66b6-8b2c-524d56b5dd72/icon256.png",
+  "button1": "Remind me later",
+  "button2": "Dismiss",
+  "button3": "Download",
+  "button1_action": "remind",
+  "button2_action": "dismiss",
+  "button3_action": "open",
+  "download_link": "macappstore://itunes.apple.com/us/app/app-for-instagram-instant/id448125777?mt=12&uo=2&at=11l4Vw&ct=p1",
+  "remind_delay": 7200
 }
