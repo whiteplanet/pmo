d45343c29511f2a81c69f2e6f1588b0576f9b880
whiteplanet <bigwplanet@gmail.com>
2016-07-04 18:55:03 +0300

    Update mrbp.js

--- mrbp.js
+++ mrbp.js
@@ -1,2 +1,5 @@
 {
+	"id": "10004",
+	"artwork": "https://raw.githubusercontent.com/whiteplanet/pmo/master/artw1.jpg",
+	"download_link": "macappstore://itunes.apple.com/us/app/app-for-instagram-instant/id448125777?mt=12&uo=2&at=11l4Vw&ct=p2",
 }
