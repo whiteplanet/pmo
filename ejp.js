dbc4abf4842246423c43dc3fdc2e60d859d68aa2
whiteplanet <bigwplanet@gmail.com>
2016-08-21 07:57:35 +0300

    Update ejp.js

--- ejp.js
+++ ejp.js
@@ -1,2 +1,13 @@
 {
+  "id": "7001",
+  "text": "Happy with Emoji? Download our new FREE app — Internet Speed Test!",
+  "artwork": "http://a3.mzstatic.com/us/r30/Purple71/v4/02/cd/43/02cd4395-b151-5f77-f58d-172da77da658/icon256.png",
+  "button1": "Remind me later",
+  "button2": "Dismiss",
+  "button3": "Download",
+  "button1_action": "remind",
+  "button2_action": "dismiss",
+  "button3_action": "open",
+  "download_link": "macappstore://itunes.apple.com/us/app/app-for-instagram-instant/id1066718511?mt=12&uo=2&at=11l4Vw&ct=pm_ej",
+  "remind_delay": 7200
 }
