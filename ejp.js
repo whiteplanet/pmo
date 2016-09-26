022466aa4e31d69ae0fc31df5cb1dfa759578876
whiteplanet <bigwplanet@gmail.com>
2016-09-26 14:24:49 +0300

    Update ejp.js

--- ejp.js
+++ ejp.js
@@ -1,13 +1,13 @@
 {
   "id": "7001",
-  "text": "Happy with Emoji? Download our new FREE app — Internet Speed Test!",
-  "artwork": "http://a3.mzstatic.com/us/r30/Purple71/v4/02/cd/43/02cd4395-b151-5f77-f58d-172da77da658/icon256.png",
+  "text": "Happy with Emoji? Download our new puzzle game!",
+  "artwork": "http://a3.mzstatic.com/us/r30/Purple22/v4/aa/49/ed/aa49edb0-0fac-b34c-cb28-a3ceffd90be6/icon256.png",
   "button1": "Remind me later",
   "button2": "Dismiss",
   "button3": "Download",
   "button1_action": "remind",
   "button2_action": "dismiss",
   "button3_action": "open",
-  "download_link": "macappstore://itunes.apple.com/us/app/app-for-instagram-instant/id1066718511?mt=12&uo=2&at=11l4Vw&ct=pm_ej",
+  "download_link": "macappstore://itunes.apple.com/us/app/app/id1128074521?mt=12&uo=2&at=11l4Vw&ct=pm_ubk",
   "remind_delay": 7200
 }
