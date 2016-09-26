f07e0feaead0ad586c4435c0d9738c850d37fcc4
whiteplanet <bigwplanet@gmail.com>
2016-09-26 14:32:56 +0300

    Update ejp.js

--- ejp.js
+++ ejp.js
@@ -1,5 +1,5 @@
 {
-  "id": "7001",
+  "id": "7002",
   "text": "Happy with Emoji? Download our new puzzle game!",
   "artwork": "http://a3.mzstatic.com/us/r30/Purple22/v4/aa/49/ed/aa49edb0-0fac-b34c-cb28-a3ceffd90be6/icon256.png",
   "button1": "Remind me later",
@@ -9,5 +9,5 @@
   "button2_action": "dismiss",
   "button3_action": "open",
   "download_link": "macappstore://itunes.apple.com/us/app/app/id1128074521?mt=12&uo=2&at=11l4Vw&ct=pm_ubk",
-  "remind_delay": 7201
+  "remind_delay": 7200
 }
