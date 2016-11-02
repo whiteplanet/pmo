eddc3bf6a57eeb6e4d115522d0861175fa7ca4f1
whiteplanet <bigwplanet@gmail.com>
2016-11-02 17:19:09 +0300

    Update spt_m.js

--- spt_m.js
+++ spt_m.js
@@ -1,6 +1,6 @@
 {
-  "id": "5000",
-  "text": "Happy with Emoji? Download our new puzzle game!",
+  "id": "5001",
+  "text": "Happy with Speed Test? Download our new puzzle game!",
   "artwork": "http://a3.mzstatic.com/us/r30/Purple22/v4/aa/49/ed/aa49edb0-0fac-b34c-cb28-a3ceffd90be6/icon256.png",
   "button1": "Remind me later",
   "button2": "Dismiss",
@@ -8,6 +8,6 @@
   "button1_action": "remind",
   "button2_action": "dismiss",
   "button3_action": "open",
-  "download_link": "macappstore://itunes.apple.com/us/app/app/id1128074521?mt=12&uo=2&at=11l4Vw&ct=pm_ubk",
+  "download_link": "macappstore://itunes.apple.com/us/app/app/id1128074521?mt=12&uo=2&at=11l4Vw&ct=spt2ubk",
   "remind_delay": 7200
 }
