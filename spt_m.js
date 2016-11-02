fbc6216a15a68b8b894596cf4af19be856ce94f3
whiteplanet <bigwplanet@gmail.com>
2016-11-02 17:13:19 +0300

    Create spt_m.js

---
+++ spt_m.js
@@ -0,0 +1,13 @@
+{
+  "id": "5000",
+  "text": "Happy with Emoji? Download our new puzzle game!",
+  "artwork": "http://a3.mzstatic.com/us/r30/Purple22/v4/aa/49/ed/aa49edb0-0fac-b34c-cb28-a3ceffd90be6/icon256.png",
+  "button1": "Remind me later",
+  "button2": "Dismiss",
+  "button3": "Download",
+  "button1_action": "remind",
+  "button2_action": "dismiss",
+  "button3_action": "open",
+  "download_link": "macappstore://itunes.apple.com/us/app/app/id1128074521?mt=12&uo=2&at=11l4Vw&ct=pm_ubk",
+  "remind_delay": 7200
+}
