286ca23a87ba238a6b5701d4f7e0600e0a319570
whiteplanet <bigwplanet@gmail.com>
2017-02-01 02:35:41 +0300

    Update spt_m.js

--- spt_m.js
+++ spt_m.js
@@ -1,5 +1,5 @@
 {
-  "id": "7005",
+  "id": "7006",
   "text": "Happy with Speed Test? Download our Emoji Emoticons app!",
   "artwork": "http://is4.mzstatic.com/image/thumb/Purple71/v4/59/b3/b7/59b3b7f4-7e30-fd17-a251-2af7aa6f5bf1/source/175x175bb.png",
   "button1": "Remind me later",
