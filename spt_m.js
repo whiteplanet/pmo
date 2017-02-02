a037556d3321c3c9a71d1aeab38a8e80d7d9bd9f
whiteplanet <bigwplanet@gmail.com>
2017-02-02 21:19:24 +0300

    Update spt_m.js

--- spt_m.js
+++ spt_m.js
@@ -1,13 +1,2 @@
 {
-  "id": "7006",
-  "text": "Happy with Speed Test? Download our Emoji Emoticons app!",
-  "artwork": "http://is4.mzstatic.com/image/thumb/Purple71/v4/59/b3/b7/59b3b7f4-7e30-fd17-a251-2af7aa6f5bf1/source/175x175bb.png",
-  "button1": "Remind me later",
-  "button2": "Dismiss",
-  "button3": "Download",
-  "button1_action": "remind",
-  "button2_action": "dismiss",
-  "button3_action": "open",
-  "download_link": "macappstore://itunes.apple.com/us/app/app/id458040553?mt=12&uo=2&at=11l4Vw&ct=pm_spt_em",
-  "remind_delay": 3600
 }
